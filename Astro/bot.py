c18c188c848f759e93c6b12c168bc567d7868c69
Jammy <50445974+Jammy108@users.noreply.github.com>
2020-04-28 10:49:42 +0100

    Fixes, moved on_ready and db/cache
    - Fixed prefix for DM
    - Moved on_ready to system cog
    - Moved db connection and cache creation to system cog

--- Astro/bot.py
+++ Astro/bot.py
@@ -4,33 +4,34 @@ from discord.ext import commands
 import asyncio
 import utils
 
-
 extensions = utils.Config.get('extensions')
 
 #Function which starts bot
 async def run():
 
-    #Creates database connection
-    await utils.db.connect()
-
-    #Creates database cache
-    await utils.Cache.create()
-
     # Creates Astro class instance
     bot = Astro()
 
+
     # Starts bot
     try:
-        await bot.start(utils.Config.get('token'))
+        token = utils.Config.get('token')
+        await bot.start(token)
     except KeyboardInterrupt:
         await db.close()
         await bot.logout()
 
 
+
+
 # Returns prefix from db cache
 async def get_prefix(bot, message):
-    prefix = utils.Cache.get_prefix(message.guild.id)
-    return commands.when_mentioned_or(prefix)(bot, message)
+    if message.guild is None:
+        return ''
+
+    prefix = utils.Cache.prefixes[message.guild.id]
+    return commands.when_mentioned_or(*prefix)(bot, message)
+
 
 
 # Bot class
@@ -47,9 +48,19 @@ class Astro(commands.Bot):
         for extension in extensions:
             self.load_extension('cogs.' + extension)
 
-    #Called when the bot is ready
-    async def on_ready(self):
-        print(f'Logged in as {self.user.name} | {self.user.id}')
+
+
+    #Override bot close function
+    async def close(self):
+
+        #Close db connection
+        await utils.db.conn.close()
+
+        #Actually shutdown bot
+        await super().close()
+
+
+
 
 
 loop = asyncio.get_event_loop()
