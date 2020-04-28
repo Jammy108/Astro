ec1c2f614b1b29ef47b35b70b5c0d1a6960defed
Jammy <50445974+Jammy108@users.noreply.github.com>
2020-04-28 10:54:43 +0100

    Importing of custom errors

--- Astro/utils/__init__.py
+++ Astro/utils/__init__.py
@@ -3,6 +3,8 @@ from .database import Database
 from .cache import Cache
 from .embed import Embed
 
+from .errors import (ModuleDisabled, CommandDisabled)
+
 Config = Config()
 db = Database()
 Cache = Cache()
