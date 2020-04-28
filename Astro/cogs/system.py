926a386fbdb200b609f3a2a87a8f7179c0a90bb3
Jammy <50445974+Jammy108@users.noreply.github.com>
2020-04-28 12:42:16 +0100

    Added more listeners
    Listeners to remove + add guilds to db and cache on join + leave

--- Astro/cogs/system.py
+++ Astro/cogs/system.py
@@ -25,6 +25,35 @@ class System(commands.Cog):
 
 
 
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+
+        default_prefix = utils.Config.get('default_prefix')
+
+        #Adds guild to caches
+        utils.Cache.prefixes[guild.id] = default_prefix
+        utils.Cache.disabled_commands[guild.id] = []
+        utils.Cache.disabled_modules[guild.id] = []
+
+        #Adds guild to db
+        await utils.db.conn.execute('INSERT INTO guilds (guild_id, prefix) VALUES ($1, $2)', guild.id, default_prefix)
+
+
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+
+        #Removes guild from cache
+        del utils.Cache.prefixes[guild.id]
+        del utils.Cache.disabled_commands[guild.id]
+        del utils.Cache.disabled_modules[guild.id]
+
+        #Removes guild from db
+        await utils.db.conn.execute('DELETE FROM guilds WHERE guild_id = $1;', guild.id)
+
+
+
+
 
 def setup(bot):
     bot.add_cog(System(bot))
